9435f7a2635dba74eaff386c9024a6eab43c75d3
AyoungWon <thwn4750@gmail.com>
2022-10-03 23:28:46 +0900

    temp: project main view

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,5 +2,7 @@ import Project from './Project';
 import Feedback from './Feedback';
 import Calendar from './Calendar';
 import MyPage from './MyPage';
+import Logo from './Logo';
+import Alarm from './Alarm';
 
-export default {Project, Feedback, Calendar, MyPage};
+export default {Project, Feedback, Calendar, MyPage, Logo, Alarm};
--- src/screens/Project/index.tsx
+++ src/screens/Project/index.tsx
@@ -1,14 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
 import React from 'react';
+import Icons from '../../components/Icons';
+import {css} from '@emotion/native';
 
 const Project = () => {
   return (
-    <View>
-      <Text>Project</Text>
-    </View>
+    <SafeAreaView style={container}>
+      <View style={header}>
+        <Icons.Logo />
+        <TouchableOpacity>
+          <Icons.Alarm color="#fff" />
+        </TouchableOpacity>
+      </View>
+      <View style={bodyContainer}>
+        <View style={wrapper}></View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Project;
+const container = css`
+  flex: 1;
+  background-color: #18181b;
+`;
+const header = css`
+  width: 100%;
+  padding: 0 20px;
+  height: 52px;
 
-const styles = StyleSheet.create({});
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const bodyContainer = css`
+  flex: 1;
+  padding: 39px 19px;
+`;
+
+const wrapper = css`
+  border: 1px solid red;
+  height: 100%;
+`;
