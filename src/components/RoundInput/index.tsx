64c9775207c1ef6000f299143e9e3f0673c84372
AyoungWon <thwn4750@gmail.com>
2022-11-01 21:47:15 +0900

    feat: Add focus when parent touched

--- src/components/RoundInput/index.tsx
+++ src/components/RoundInput/index.tsx
@@ -1,23 +1,33 @@
 import {View, TextInputProps} from 'react-native';
-import React from 'react';
+import React, {useRef} from 'react';
 import DefaultInput from '../DefaultInput';
 import {css} from '@emotion/native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native-gesture-handler';
 import Icons from '../Icons';
 
 type Props = TextInputProps & {
   onDelete?: () => void;
 };
 const RoundInput = ({onDelete, style, ...rest}: Props) => {
+  const inputRef = useRef<TextInput>(null);
+  const handleClickInputParent = () => {
+    if (inputRef.current) inputRef.current.focus();
+  };
   return (
-    <View style={[container, style]}>
-      <DefaultInput {...rest} style={input} />
+    <TouchableWithoutFeedback
+      style={[container, style]}
+      onPress={handleClickInputParent}>
+      <DefaultInput {...rest} style={input} inputRef={inputRef} />
       {onDelete && (
         <TouchableOpacity onPress={onDelete} style={deleteButton}>
           <Icons.Close color="#535358" />
         </TouchableOpacity>
       )}
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -32,7 +42,7 @@ const container = css`
   min-height: 42px;
 `;
 const input = css`
-  flex: 1;
+  height: 100%;
 `;
 const deleteButton = css`
   width: 24px;
