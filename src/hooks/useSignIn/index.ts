f433dc04689aff57a84c4dc8ac8880c92c4bf4ea
AyoungWon <thwn4750@gmail.com>
2022-12-11 19:56:03 +0900

    temp: set apple login

--- src/hooks/useSignIn/index.ts
+++ src/hooks/useSignIn/index.ts
@@ -7,7 +7,7 @@ import api from '../../api';
 import {SignInResType, SignInSuccessResType} from '../../api/auth';
 import userStore from '../../store/userStore';
 import {saveSessions} from '../useAxiosInterceptor';
-
+import {appleAuth} from '@invertase/react-native-apple-authentication';
 GoogleSignin.configure({
   iosClientId: Config.GOOGLE_IOS_CLIENT_ID,
 });
@@ -52,38 +52,99 @@ const useSignIn = () => {
       }
     }
   }, []);
+  const appleSignIn = useCallback(
+    async ({signUpCallback}: CallbacksType) => {
+      let idToken: string = '';
+      if (Platform.OS === 'ios') {
+        try {
+          const appleAuthRequestResponse = await appleAuth.performRequest({
+            requestedOperation: appleAuth.Operation.LOGIN,
+            requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+          });
 
-  const appleSignIn = useCallback(async ({signUpCallback}: CallbacksType) => {
-    let idToken: string = '';
-    if (Platform.OS === 'ios') {
-      // try {
-      //   const appleAuthRequestResponse = await appleAuth.performRequest({
-      //     requestedOperation: appleAuth.Operation.LOGIN,
-      //     requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
-      //   });
-      //   const {user, email, nonce, identityToken, realUserStatus} =
-      //     appleAuthRequestResponse;
-      //   const credentialState = await appleAuth.getCredentialStateForUser(
-      //     user,
-      //   );
-      //   //todo idtoken null일 때 토큰 가져오기
-      //   idToken = identityToken || '';
-      // } catch (error: any) {
-      //   console.log(error);
-      //   if (error.code !== '1001') {
-      //     Toast.show({
-      //       type: 'error',
-      //       text1: error.message,
+          console.log('????', appleAuthRequestResponse);
+
+          const {user, email, nonce, identityToken, realUserStatus} =
+            appleAuthRequestResponse;
+
+          const credentialState = await appleAuth.getCredentialStateForUser(
+            user,
+          );
+          console.log(appleAuthRequestResponse, 'res');
+          if (credentialState === appleAuth.State.AUTHORIZED) {
+            console.log('user is authenticated');
+          }
+
+          //todo idtoken null일 때 토큰 가져오기
+          idToken = identityToken || '';
+        } catch (error: any) {
+          console.log(error);
+          if (error.code !== '1001') {
+            // Toast.show({
+            //   type: 'error',
+            //   text1: error.message,
+            // });
+          }
+          return;
+        }
+      }
+
+      // if (Platform.OS === 'android') {
+      //   // todo require API +19 toast
+      //   if (!appleAuthAndroid.isSupported) return;
+
+      //   try {
+      //     appleAuthAndroid.configure({
+      //       clientId: Config.APPLE_WEB_CLIENT_ID,
+      //       redirectUri: 'https://earntube.io/v1/user/apple/callback',
       //     });
+
+      //     const response = await appleAuthAndroid.signIn();
+      //     if (response) {
+      //       const code = response.code;
+      //       const id_token = response.id_token;
+      //       const user = response.user;
+      //       const state = response.state;
+      //       //todo idtoken null일 때 토큰 가져오기
+      //       idToken = id_token || '';
+      //       // console.log('Got auth code', code);
+      //       // console.log('Got id_token', id_token);
+      //       // console.log('Got user', user);
+      //       // console.log('Got state', state);
+      //     }
+      //   } catch (error: any) {
+      //     console.log(error);
+      //     if (error.code !== '1001') {
+      //       // Toast.show({
+      //       //   type: 'error',
+      //       //   text1: error.message,
+      //       // });
+      //     }
+      //     return;
       //   }
-      //   return;
+      // }
+      // const {token, permission} = await requestUserPermission();
+      // const device = await getUniqueId();
+      // const {data} = await api.user.loginByApple({
+      //   idToken,
+      //   device,
+      //   token,
+      //   permission,
+      // });
+      // if (isLogin(data)) {
+      //   await saveSessions(data).then(getProfileWithMission);
+      // } else {
+      //   dispatch(setSignUpSession(data));
+      //   signUpCallback();
       // }
       // appleAuth.performRequest({
       //   requestedOperation: appleAuth.Operation.LOGOUT,
       // });
-    }
-  }, []);
-
+    },
+    [
+      /* dispatch*/
+    ],
+  );
   return {
     googleSignIn,
     appleSignIn,
