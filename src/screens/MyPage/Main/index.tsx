894346f9d8529292f4bfaa4c359d45f7c95a0c08
AyoungWon <thwn4750@gmail.com>
2023-01-29 17:06:28 +0900

    feat: set logout alert modal

--- src/screens/MyPage/Main/index.tsx
+++ src/screens/MyPage/Main/index.tsx
@@ -1,13 +1,14 @@
 import {Image, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Background,
   Header,
   DefaultText as Text,
   RoundSquareButton,
   Icons,
+  DefaultModal as Modal,
 } from '../../../components';
-import userStore from '../../../store/userStore';
+
 import {css} from '@emotion/native';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {RootStackParamList} from '../../../navigation/main';
@@ -17,17 +18,26 @@ import {useSignOut} from '../../../hooks';
 type Props = UserProfileType;
 const Main = ({name, jobFieldDetail, email, profileImage}: Props) => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const [openModal, setOpenModal] = useState<
+    undefined | 'edit' | 'logout' | 'withdrawal'
+  >();
   const {signOutUser} = useSignOut();
   // console.log(field, name, profileImage, email);
 
   const handleClickPolicy = () => navigation.navigate('Policy');
   const handleClickTerms = () => navigation.navigate('Terms');
-  const handleClickLogout = () => signOutUser();
+  const handleClickLogout = () => {
+    setOpenModal(undefined);
+    setTimeout(() => {
+      signOutUser();
+    }, 500);
+  };
   const handleClickWithdrawal = () => {};
   const handleClickProfileDetail = () =>
     navigation.navigate('MyPage', {
       type: 'detail',
     });
+
   return (
     <Background>
       <Header title="My" />
@@ -67,7 +77,7 @@ const Main = ({name, jobFieldDetail, email, profileImage}: Props) => {
             style={button}
             type="dark"
             size="m"
-            onPress={handleClickLogout}>
+            onPress={() => setOpenModal('logout')}>
             로그아웃
           </RoundSquareButton>
           <RoundSquareButton
@@ -79,6 +89,30 @@ const Main = ({name, jobFieldDetail, email, profileImage}: Props) => {
           </RoundSquareButton>
         </View>
       </View>
+      {openModal === 'logout' && (
+        <Modal isVisible={true} onClose={() => setOpenModal(undefined)}>
+          <View>
+            <Text style={modalTitle}>로그아웃</Text>
+            <Text style={modalSubtitle}>로그아웃 하시겠어요?</Text>
+            <View style={[rowFlex]}>
+              <RoundSquareButton
+                type="dark"
+                size="m"
+                style={[smallButton, firstButton]}
+                onPress={() => setOpenModal(undefined)}>
+                취소
+              </RoundSquareButton>
+              <RoundSquareButton
+                type="primary"
+                size="m"
+                style={[smallButton]}
+                onPress={handleClickLogout}>
+                로그아웃
+              </RoundSquareButton>
+            </View>
+          </View>
+        </Modal>
+      )}
     </Background>
   );
 };
@@ -128,4 +162,32 @@ const rotate = css`
   margin-left: auto;
 `;
 
+const rowFlex = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 8px;
+`;
+
+const smallButton = css`
+  flex: 1;
+  min-width: 0;
+`;
+
+const firstButton = css`
+  margin-right: 8px;
+  border: 1px solid #35353a;
+`;
+
+const modalTitle = css`
+  font-weight: 600;
+  font-size: 24px;
+`;
+
+const modalSubtitle = css`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  margin: 32px 0 40px;
+`;
 export default Main;
